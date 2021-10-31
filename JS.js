08338e31159a7f59e809f4a4fce928aa8a4b1fe5
Artem <93066493+Ssecond@users.noreply.github.com>
2021-10-31 17:35:42 +1000

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,28 @@
+﻿"use strict"
+let adress = "";
+function pretendingOfSanding(buttonId, inputId) {
+	adress = document.getElementById(inputId).value;
+	if (adress.includes('@') && adress.includes('.'))
+	{
+		console.log(`Отправка произошла по адрессу ${adress}`);
+
+		// Как бы "отправка письма"
+
+		document.getElementById(buttonId).style.background = "green";
+		setTimeout(messageSent, 500);
+
+	}
+	else
+	{
+		console.log("Отправка не произошла, не указан адресс или же он неверен.");
+		document.getElementById(buttonId).style.background = "red";
+		setTimeout(messageSent, 300);
+	}
+
+	function messageSent() {
+		document.getElementById(buttonId).style.background = "rgb(212,75,56)";
+	}
+}
+function pageUp() {
+	  window.scrollTo(0, 0);
+}
